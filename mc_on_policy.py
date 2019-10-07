3cf26b2475087ef1ca8d77155ad7bd789a00e23d
batra98 <batragaurav2616@gmail.com>
2019-10-08 00:20:03 +0530

    on_policy

--- mc_on_policy.py
+++ mc_on_policy.py
@@ -8,32 +8,72 @@ from gym_tictactoe.env import TicTacToeEnv, set_log_level_by, agent_by_mark,\
 
 def make_epsilon_greedy_policy(Q,epsilon,nA):
 
-	def fn(obs):
-		non_zero = np.nonzero(obs)
+	def fn(obs,available_actions):
+		# non_zero = np.nonzero(obs)
 		
-		A = np.zeros(nA,dtype = float)
+		# A = np.zeros(nA,dtype = float)
 		
-		# print(non_zero)
+		# # print(non_zero)
 
-		for i in range(nA):
-			if i not in non_zero[0]:
-				A[i] = 1.0
+		# for i in range(nA):
+		# 	if i not in non_zero[0]:
+		# 		A[i] = 1.0
+
+		# # print(A)
+		# A = A*epsilon/(nA-len(non_zero[0]))
+
+		# if len(non_zero[0]) != 0:
+		# 	best_action = non_zero[0][0]
+		# 	for i in range(nA):
+		# 		if i not in non_zero[0]:
+		# 			if Q[obs][i] >= Q[obs][best_action]:
+		# 				best_action = i
+		# else:
+
+		# 	best_action = np.argmax(Q[obs])
+		# A[best_action] += (1 - epsilon)
+		# # print(A)
+
+		# A = np.ones(len(available_actions),dtype = float)*epsilon/(len(available_actions))
+		# A = []
+		# for i in range(nA):
+		# 	if i not in available_actions:
+		# 		A.append(0.0)
+		# 	else:
+		# 		A.append(epsilon/(len(available_actions)))
+
+		# A = np.array(A)
+
+		# # print("Q(s,a)",end = " ")
+		# # print(Q[obs])
+
+		# best_action = available_actions[0]
+		# for actions in available_actions:
+		# 	if Q[obs][actions] >= Q[obs][best_action]:
+		# 		best_action = actions
+
+		# A[best_action] += (1- epsilon)
 
 		# print(A)
-		A = A*epsilon/(nA-len(non_zero[0]))
 
-		if len(non_zero[0]) != 0:
-			best_action = non_zero[0][0]
-			for i in range(nA):
-				if i not in non_zero[0]:
-					if Q[obs][i] >= Q[obs][best_action]:
-						best_action = i
+		# return A
+
+		assert len(available_actions) > 0
+
+		e = np.random.random()
+		if e < epsilon:
+			A = np.random.choice(available_actions)
 		else:
+			best_action = available_actions[0]
+			for actions in available_actions:
+				if Q[obs][actions] >= Q[obs][best_action]:
+					best_action = actions
+			A = best_action
 
-			best_action = np.argmax(Q[obs])
-		A[best_action] += (1 - epsilon)
-		# print(A)
+
+
 		return A
+
 
 	return fn 
 
@@ -62,17 +102,19 @@ def generate_episode(env,policy):
 			
 		# else:
 
-		action = np.random.choice(available_actions)
-
-		# probability = policy(state[0])
+		# action = np.random.choice(available_actions)
+		# print(available_actions)
+		action = policy(state[0],available_actions)
 		# action = np.random.choice(np.arange(len(probability)),p = probability)
 		# print(action)
 
-		nstate,reward,done,_ = env.step(int(action))
+		nstate,reward,done,_ = env.step(action)
 		# print((state,action,reward))
 		episodes.append((state[0],action,reward))
 		state = nstate
 		iteration += 1
+
+		# env.render()
 
 		# _,mark = state
 		start_mark = next_mark(start_mark)
@@ -124,8 +166,8 @@ def mc_control_on_policy(env,num_episodes,discount_factor = 0.1,epsilon = 0.1):
 
 
 env = TicTacToeEnv(show_number = True)
-
-Q,policy = mc_control_on_policy(env,num_episodes = 50000,epsilon = 0.1)
+# env.render()
+Q,policy = mc_control_on_policy(env,num_episodes = 50000,epsilon = 0.5)
 
 
 
